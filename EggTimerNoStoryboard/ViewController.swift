80af523e04f2667f25174d11dbdbe3b5b5d3da60
Evgeniy Kotovich <sxekot@gmail.com>
2022-04-06 23:25:14 +0300

    Initial Commit

---
+++ EggTimerNoStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimerNoStoryboard
+//
+//  Created by Eugene Kotovich on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
